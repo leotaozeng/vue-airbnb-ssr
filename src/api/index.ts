4e3134f37d1d426a9e4d0a4cbb903836e5f2b605
Leo Zeng <qingchun1995@gmail.com>
2022-05-03 17:09:09 +0800

    feat: :sparkles: mock API

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,3 +1,24 @@
+import IndexedDB from '@/utils/indexedDB';
+
+const airbnbDB = new IndexedDB('airbnb');
+
+// * Mock接口：获取 elephant object store
+export const getElephant = async () => {
+  try {
+    await airbnbDB.open('elephant', 'id', ['nose', 'ear']);
+    const result = await airbnbDB.getList('elephant');
+
+    return {
+      code: '000000',
+      message: '操作成功',
+      result,
+      success: true
+    };
+  } catch (error) {
+    console.error('getElephant Error', error);
+  }
+};
+
 // * Mock接口：保存当前语言包
 export const saveLanguageApi = () => {};
 
--- src/pages/Home/index.vue
+++ src/pages/Home/index.vue
@@ -1,45 +1,20 @@
 <script setup lang="ts">
-import IndexedDB from '../../utils/indexedDB';
+import { getElephant } from '@/api';
 
-const airbnbDB = new IndexedDB('airbnb');
-airbnbDB.open('elephant', 'id', ['nose', 'ear']);
+async function fetchElephant() {
+  try {
+    const response = await getElephant();
+    console.log(response);
+  } catch (error) {
+    console.error('fetchElephant Error', error);
+  }
+}
 
-const addDB = () => {
-  airbnbDB.addItem('elephant', {
-    nose: '20m',
-    ear: 'small'
-  });
-};
-
-const updateDB = () => {
-  airbnbDB.updateItem('elephant', {
-    nose: '10m',
-    ear: 'medium',
-    id: 1
-  });
-};
-
-const deleteDB = () => {
-  airbnbDB.removeItem('elephant', 1);
-};
-
-const getAllData = () => {
-  airbnbDB.getList('elephant');
-};
-
-const getData = () => {
-  airbnbDB.getItem('elephant', 4);
-};
+fetchElephant();
 </script>
 
 <template>
   <div>
-    <el-button type="primary" @click="addDB">Add</el-button>
-    <el-button type="primary" @click="updateDB">Update</el-button>
-    <el-button type="primary" @click="deleteDB">Delete</el-button>
-    <el-button type="primary" @click="getAllData">Get All</el-button>
-    <el-button type="primary" @click="getData">Get Data</el-button>
-    <br /><br />
     <el-date-picker type="datetime" placeholder="Select date and time" />
   </div>
 </template>
