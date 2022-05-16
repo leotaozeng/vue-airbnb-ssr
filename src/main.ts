20749e6b74b5aeddd69cffa457837160409b23f7
Leo Zeng <qingchun1995@gmail.com>
2022-05-17 00:11:31 +0800

    chore: :bulb: update a comment in main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,8 @@ import App from './App.vue';
 import i18n from './i18n';
 import { createSSRRouter } from './router';
 
-// * SSR requires a fresh app instance per request, therefore we export a function
-// * that creates a fresh app instance. If using Vuex, we'd also be creating a fresh store here.
+// * SSR requires a fresh app instance per request, therefore we export a function that creates a fresh app instance.
+// * If using Vuex, we'd also be creating a fresh store here.
 // * exports env-agnostic (universal) app code
 export function createApp() {
   const app = createSSRApp(App);
