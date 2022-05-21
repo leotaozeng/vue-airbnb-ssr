df1f944818488f9cdd0ee79c21cf362b7802d0d4
Leo Zeng <qingchun1995@gmail.com>
2022-05-21 18:59:33 +0800

    build: tailwind customization

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,23 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const colors = require('tailwindcss/colors');
-
 module.exports = {
   important: true,
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {
       fontFamily: {
-        sans: 'Circular, -apple-system, BlinkMacSystemFont, Roboto, "Helvetica Neue", PingFang-SC, "Hiragino Sans GB", 微软雅黑, "Microsoft YaHei", "Heiti SC", sans-serif'
+        sans: 'Circular, -apple-system, BlinkMacSystemFont, Roboto, Helvetica Neue, sans-serif',
+        heiti:
+          'Circular, PingFang-SC, "Hiragino Sans GB", 微软雅黑, "Microsoft YaHei", "Heiti SC"'
       },
       colors: {
-        muted: '#767676',
-        'primary-gray': '#484848',
+        gray: {
+          muted: '#767676',
+          dark: '#484848'
+        },
         neutral: {
           150: 'rgb(235 235 235)'
         }
       }
     }
   },
-  plugins: []
+  plugins: [require('@tailwindcss/line-clamp')]
 };
