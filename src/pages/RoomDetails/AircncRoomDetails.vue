ecb39f4c760a9e640b29298e6e6565f05fe2a357
Leo Zeng <qingchun1995@gmail.com>
2022-05-27 16:57:27 +0800

    refactor: update the UI of the carousel component on the room details page

--- src/pages/RoomDetails/AircncRoomDetails.vue
+++ src/pages/RoomDetails/AircncRoomDetails.vue
@@ -16,15 +16,26 @@ onBeforeMount(() => {
 
 <template>
   <div v-if="roomsStore.roomDetails">
-    <el-carousel type="card" indicator-position="none" :autoplay="false">
-      <el-carousel-item v-for="img in roomsStore.roomDetails.imgs" :key="img">
-        <el-image :src="img" class="w-full" />
-      </el-carousel-item>
-    </el-carousel>
+    <div v-if="roomsStore.roomDetails.imgs.length > 0" class="slide-container">
+      <el-carousel
+        class="slide py-4"
+        type="card"
+        indicator-position="none"
+        height="406px"
+        :autoplay="false">
+        <el-carousel-item
+          class="slide-item rounded-md"
+          v-for="img in roomsStore.roomDetails.imgs"
+          :key="img">
+          <el-image :src="img" class="w-full" />
+        </el-carousel-item>
+      </el-carousel>
+    </div>
 
     <div class="room-details flex">
       <div class="details">
-        <section>
+        <section
+          v-if="roomsStore.roomDetails.info && roomsStore.roomDetails.title">
           <!-- Title -->
           <h1 class="title mb-2 py-1.5 font-extrabold text-gray-dark font-sans">
             {{ roomsStore.roomDetails.title }}
@@ -32,22 +43,30 @@ onBeforeMount(() => {
 
           <!-- Room Info -->
           <div class="flex mb-4">
-            <div class="flex items-center bedrooms mr-4">
+            <div
+              v-if="roomsStore.roomDetails.info.room > 0"
+              class="flex items-center bedrooms mr-4">
               <span class="mr-2 font-airmoji">󰄄</span>
               <span>{{ roomsStore.roomDetails.info.room }} 间卧室</span>
             </div>
 
-            <div class="flex items-center beds mr-4">
+            <div
+              v-if="roomsStore.roomDetails.info.bed > 0"
+              class="flex items-center beds mr-4">
               <span class="mr-2 font-airmoji">󰄃</span>
               <span>{{ roomsStore.roomDetails.info.bed }} 张床</span>
             </div>
 
-            <div class="flex items-center baths mr-4">
+            <div
+              v-if="roomsStore.roomDetails.info.toilet > 0"
+              class="flex items-center baths mr-4">
               <span class="mr-2 font-airmoji">󰄁</span>
               <span>{{ roomsStore.roomDetails.info.toilet }} 间卫生间</span>
             </div>
 
-            <div class="flex items-center guests">
+            <div
+              v-if="roomsStore.roomDetails.info.liveNumber > 0"
+              class="flex items-center guests">
               <span class="mr-2 font-airmoji">󰄂</span>
               <span>可住 {{ roomsStore.roomDetails.info.liveNumber }} 人</span>
             </div>
@@ -56,6 +75,7 @@ onBeforeMount(() => {
           <!-- Tags -->
           <div class="tags mb-6">
             <el-tag
+              v-if="roomsStore.roomDetails.info.remarks > 0"
               class="mb-2 mr-1 rounded-2xl text-green-dark font-extrabold"
               type="success">
               {{ roomsStore.roomDetails.info.remarks }}条评论
@@ -83,11 +103,12 @@ onBeforeMount(() => {
           <div class="line my-4"></div>
         </section>
 
-        <section>
+        <section
+          v-if="roomsStore.roomDetails.info && roomsStore.roomDetails.owner">
           <!-- * Owner Details -->
           <div class="owner-details flex">
             <!-- * Avatar -->
-            <div>
+            <div v-if="roomsStore.roomDetails.owner.avatar">
               <el-avatar
                 :src="roomsStore.roomDetails.owner.avatar"
                 :size="64" />
@@ -95,7 +116,9 @@ onBeforeMount(() => {
 
             <!-- * Info -->
             <div class="ml-4 py-2">
-              <p class="mb-2 font-extrabold text-base text-gray-dark">
+              <p
+                v-if="roomsStore.roomDetails.owner.name"
+                class="mb-2 font-extrabold text-base text-gray-dark">
                 房东：{{ roomsStore.roomDetails.owner.name }}
               </p>
 
@@ -162,6 +185,13 @@ onBeforeMount(() => {
 
 <style lang="scss" scoped>
 @import '@/assets/scss/abstracts/mixins.scss';
+
+.slide-container {
+  .slide-item {
+    border: 10px solid #fff;
+    box-shadow: 0 2px 5px 0 rgb(0 0 0 / 20%);
+  }
+}
 
 .room-details {
   @include secondary-wrapper;
