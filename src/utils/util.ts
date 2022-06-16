a054d40251487e96ee142f96ac7f715b06a2e880
Leo Zeng <qingchun1995@gmail.com>
2022-06-16 19:23:57 +0800

    feat: feat: add two utility functions: parseDate, dateDiff

--- src/utils/util.ts
+++ src/utils/util.ts
@@ -19,3 +19,17 @@ export function getCookie(cname: string) {
 export function daysInMonth(month: number, year: number) {
   return new Date(year, month, 0).getDate();
 }
+
+export function parseDate(date: string) {
+  if (!date) return;
+  const mdy = date.split('/'); // YYYY/MM/DD
+  return new Date(+mdy[0], +mdy[1] - 1, +mdy[2]);
+}
+
+export function dateDiff(first: Date, second: Date) {
+  // Take the difference between the dates and divide by milliseconds per day.
+  // Round to nearest whole number to deal with DST.
+  return Math.round(
+    (second.getTime() - first.getTime()) / (1000 * 60 * 60 * 24)
+  );
+}
