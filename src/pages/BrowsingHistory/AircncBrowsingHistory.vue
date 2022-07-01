03ede094a0feb876afa8984c8ed7ed7d4ea31399
Leo Zeng <qingchun1995@gmail.com>
2022-07-01 12:13:09 +0800

    feat: get a history list

--- src/pages/BrowsingHistory/AircncBrowsingHistory.vue
+++ src/pages/BrowsingHistory/AircncBrowsingHistory.vue
@@ -1,7 +1,27 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { fetchHistoryListAPI } from '@/api/history';
+import { ElMessage } from 'element-plus';
+
+const history = ref([]);
+
+async function getHistoryList() {
+  const response = await fetchHistoryListAPI();
+  if (response) {
+    const { message, result, success } = response;
+    success
+      ? ElMessage({ message, type: 'success', showClose: true })
+      : ElMessage({ message, type: 'error', showClose: true });
+    if (result) history.value = result;
+  }
+}
+
+onBeforeMount(() => {
+  getHistoryList();
+});
+</script>
 
 <template>
-  <div>browsing history</div>
+  <div>{{ history }}</div>
 </template>
 
 <style lang="scss" scoped></style>
